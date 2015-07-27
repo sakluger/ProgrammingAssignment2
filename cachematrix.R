6f7e1eff46eb31612febb6a4ae2d390d4d40d67e
sakluger <sakluger@gmail.com>
2015-07-27 11:34:30 -0700

    Small bug fix for ProgrammingAssignment2 for  Coursera 'R Programming' course

--- cachematrix.R
+++ cachematrix.R
@@ -31,7 +31,7 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setinverse <- function(inverse) m <<- inverse
     getinverse <- function() m
-    list(set = set, get = get, setinverse = setinverse, get = getinverse)
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
